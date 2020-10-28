37ffc68759a89661e17340af4fbc1a1e7284b534
ramya vummaneni <ramyavummaneni@ramyas-MacBook-Pro.local>
2020-10-28 11:37:14 +0530

    Updates

---
+++ precache-manifest.7d19750fd95a162a7d7c8688173c0715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502e6c09dc089c49996f02f4339a093d",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "90914bbc4ff8cbdfcda8",
+    "url": "/seasons/static/css/main.8bce5068.chunk.css"
+  },
+  {
+    "revision": "0bf3340bcb487386564d",
+    "url": "/seasons/static/js/2.4f740efb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.4f740efb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90914bbc4ff8cbdfcda8",
+    "url": "/seasons/static/js/main.067c553e.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
